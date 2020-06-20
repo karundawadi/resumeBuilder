8d2da9a6d7ca854d3a4427e3707715671009e1c5
karun.dawadi@mavs.uta.edu <karun.dawadi@mavs.uta.edu>
2020-06-20 16:30:43 -0500

    Portal needs to be made

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,92 @@
+import React, { Component,useState } from 'react'
+import 'fontsource-roboto'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import EmailIcon from '@material-ui/icons/Email'
+import { makeStyles} from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { Grid, TextField, Modal } from '@material-ui/core'
+import EmailIconClicked from './emailPopUp'
+
+//Obtained from https://material-ui.com/customization/color/
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#26a69a',
+      },
+      secondary: {
+        main: '#66bb6a',
+      },
+    },
+  });
+
+  //Did not make the sides hidable as that woudl make the overall look bad in small phones
+const useStyles = makeStyles(() => ({
+    root: {
+      flexGrow: 1,
+      background: theme.palette.primary.main,
+      fontSize: 'large',
+    },
+    writings:{
+        alignContent: 'center',
+    },
+    icons:{
+        size: "inherit",
+    }
+  }))
+
+function NavigationBar(props) {
+    const classes = useStyles()
+    const [open, setOpen] = useState(false)
+    return (
+        <React.Fragment>
+            <AppBar className={classes.root}>
+                    <Toolbar>
+                        <Grid container spacing={0}>
+                            <Grid item xs={2}>
+                                <EmailIcon className={classes.icons} onClick={()=>{
+                                    setOpen(true)
+                                    //Learn more about portal here
+                                    console.log("Amifo")
+                                }}>
+                                </EmailIcon>
+                            </Grid> 
+                            <Grid item xs={7}>
+                                <Typography className={classes.writings} variant="h5">
+                                     Karun Dawadi 
+                                </Typography>
+                            </Grid> 
+                            <Grid item xs={3}>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={1}>
+                                        <TwitterIcon className={classes.icons} onClick={()=>{
+                                            let url = "http://www.twitter.com"
+                                            window.open(url)
+                                        }}></TwitterIcon>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <LinkedInIcon className={classes.icons} onClick={()=>{
+                                            let url = "http://www.linkedin.com"
+                                            window.open(url)
+                                        }}></LinkedInIcon>
+                                    </Grid>
+                                    <Grid item xs={1}>
+                                        <GitHubIcon className={classes.icons} onClick={()=>{
+                                            let url = "http://www.github.com"
+                                            window.open(url)
+                                        }}></GitHubIcon>
+                                    </Grid>
+                                </Grid>
+                            </Grid> 
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+        </React.Fragment>
+    )
+}
+
+export default NavigationBar
---
+++ src/Components/emailPopUp.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import Dialog from '@material-ui/core/Dialog'
+import Button from '@material-ui/core/Button'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import { Grid, TextField } from '@material-ui/core'
+
+//To handle email input from the user 
+//Props only has the email 
+function EmailIconClicked(props) {
+    const [open, setOpen] = useState(props.buttonCliked)
+
+    const handleClose = ()=>{
+        setOpen(false)
+    }
+    return (
+        <React.Fragment>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>
+                    Thank you.
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Thank you for showing interest in me. Please compose a message for me.
+                    </DialogContentText>
+                    <TextField autoFocus margin='dense' label="I am hiring you." type="text"/>
+                    <Button onClick={()=>{
+                        window.open('mailto:'+"karundawadi00@gmail.com"+'?cc='+"Ola"+'&subject='+"Hey"+'&body='+"emailBody", '_self');
+                        setOpen(false)
+                    }}>Send</Button>
+                </DialogContent>
+            </Dialog>
+        </React.Fragment>
+    )
+}
+
+export default EmailIconClicked
