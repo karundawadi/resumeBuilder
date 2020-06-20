2a8c02d71e098146ddd19d52d360c1285d7911be
karun.dawadi@mavs.uta.edu <karun.dawadi@mavs.uta.edu>
2020-06-20 13:41:40 -0500

    Project reset to complete it properly

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React,{useState} from 'react'
-import BodyPart from './components/bodypart'
-import PopUpPart from './components/popuppart'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [displayPopUp,changeValue] = useState(true)
-  const [userDetails,changeUserDetails] = useState({'':''})
-
-  function handleChange(newValue,newUserDetails){
-    changeValue(newValue)
-    changeUserDetails(newUserDetails)
-  }
   return (
-      <React.Fragment>
-      <PopUpPart onChange = {handleChange} currentState = {displayPopUp}/>
-      <BodyPart userDetailsTransferred={userDetails}/>
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
