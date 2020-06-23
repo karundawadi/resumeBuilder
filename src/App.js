f9fe31475c009fe9dcf66cb5d7a3e3b4ad7e2dcf
karun.dawadi@mavs.uta.edu <karun.dawadi@mavs.uta.edu>
2020-06-23 16:25:33 -0500

    Need to use ReactToPrint.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import NavigationBar from './Components/NavigationBar'
 import BodyPart from './Components/BodyPart'
 import ShowDialog from './Components/popUpPart'
 import { Button } from '@material-ui/core'
+import ReactToPrint from 'react-to-print';
+
 import './App.css';
 
-
-function App() {
+function AppC() {
     const [showDialog, setDialog] = useState(true)
     const [userDetails,changeUserDetails] = useState({'':''})
-
     function handleChange(status, newUserDetails){
       setDialog(status)
       changeUserDetails(newUserDetails)
@@ -20,9 +20,21 @@ function App() {
       <ShowDialog showDialog={showDialog} onChange={handleChange}/>
       <NavigationBar userDetails={userDetails}/>
       <BodyPart/>
-      <Button>Save</Button>
     </React.Fragment>
   );
+}
+function App(){
+  const componentRef = React.useRef();
+  return(
+    <React.Fragment>
+      <AppC/>
+      {/* <ReactToPrint documentTitle="Test" copyStyles={true} 
+      trigger={() => <button>Print this out!</button>}
+      content={() => componentRef.current}
+      />
+      <ComponentToPrint ref={componentRef} /> */}
+    </React.Fragment>
+  )
 }
 
 export default App;
