fec73df914c6f2f6be7d01da05f739d73ea25ec2
karun.dawadi@mavs.uta.edu <karun.dawadi@mavs.uta.edu>
2020-06-23 00:22:05 -0500

    Need to fix the body.

--- src/Components/BodyPart.js
+++ src/Components/BodyPart.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React,{Fragment} from 'react'
+import { Grid, TextField, Paper } from '@material-ui/core'
+import { makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+    body: {
+      flexGrow: 1,
+      padding :"2%",
+    },
+  }))
 
 function BodyPart() {
+    const classes = useStyles()
     return (
-        <div>
-            
-        </div>
+        <Fragment style={classes.body}>
+           <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper>
+                        <ul>
+                           <li>OIa</li>
+                        </ul>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Fragment>
     )
 }
 
